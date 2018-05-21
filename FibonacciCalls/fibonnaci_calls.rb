463cd6047947db715352d704b7396534b1aeb039
Victor <victorcorcos@gmail.com>
2018-05-21 19:48:22 -0300

    Add other Fibonacci functions

--- FibonacciCalls/fibonnaci_calls.rb
+++ FibonacciCalls/fibonnaci_calls.rb
@@ -14,3 +14,38 @@ test_cases.times do
   number = STDIN.gets.chomp.to_i
   fibonacci_calls(number)
 end
+
+
+###
+# More Fibonacci Functions below...
+###
+# Top-Down
+# Bottom-Up with Array
+# Bottom-Up with just Auxiliary Variables (Space Saving)
+
+def fibonacci_top_down(n)
+  return n if n < 2
+  return fibonacci_top_down(n - 1) + fibonacci_top_down(n - 2)
+end
+
+def fibonacci_bottom_up_array(n)
+  fib_results = [0, 1]
+  (2..n+1).each do |index|
+    fib_results.push(fib_results[index - 1] + fib_results[index - 2])
+  end
+  fib_results[n]
+end
+
+def fibonacci_bottom_up_variables(n)
+  first_before = 1
+  second_before = 0
+  result = 0
+  return second_before if n == 0
+  return first_before if n == 1
+  (2..n).each do
+    result = first_before + second_before
+    second_before = first_before
+    first_before = result
+  end
+  result
+end
--- Sum/sum.rb
+++ Sum/sum.rb
@@ -1,2 +1,2 @@
 n = STDIN.gets.chomp.to_i + STDIN.gets.chomp.to_i
-puts 'SOMA = ' << n.to_s
+print 'SOMA = ' << n.to_s << "\n"
